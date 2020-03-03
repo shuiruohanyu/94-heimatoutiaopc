84d29a65e9057e7990cc34abb4d23026a7fc19fb
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-03 12:05:10 +0800

    主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -13,11 +13,11 @@
     <el-col class='right' :span="12">
         <!-- 再次放置一个 row组件  align属性设置垂直对齐方式  justify设置 水平对齐属性-->
         <el-row type='flex' justify="end" align="middle">
-          <img src="http://img0.imgtn.bdimg.com/it/u=4090061760,3566002114&fm=26&gp=0.jpg" alt="">
+          <img :src="userInfo.photo" alt="">
            <!-- 下拉菜单 -->
            <el-dropdown trigger='click'>
                <!-- 显示的内容 -->
-               <span>水若寒宇</span>
+               <span>{{ userInfo.name }}</span>
                 <!-- 下拉内容需要做具名插槽dropdown  el-dropdown-menu是专门做下拉的组件 -->
                 <el-dropdown-menu slot="dropdown" >
                    <!-- 下拉选项 el-dropdown-item 作为下拉选项组件-->
@@ -33,7 +33,24 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {} // 用户个人信息
+    }
+  },
+  created () {
+    const token = localStorage.getItem('user-token') // 从兜里拿钥匙 也就是从缓存中取token
+    //   获取用户的个人信息
+    this.$axios({
+      url: '/user/profile', // 请求地址
+      headers: {
+        Authorization: `Bearer ${token}` // 格式要求 Bearer +token
+      } // 请求头参数 headers放置请求头参数
+    }).then(result => {
+      // 如果加载成功了 我们要将数据赋值给 userInfo
+      this.userInfo = result.data.data
+    })
+  }
 }
 </script>
 
