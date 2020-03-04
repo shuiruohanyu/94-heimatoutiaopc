a36f48022ad3a5682a110320e87b1f637b17a1e6
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-04 11:48:16 +0800

    黑马头条PC-评论列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,10 @@ const routes = [
       { //
         path: '', // 二级路由的path 什么都不写 代表二级路由的默认组件 如果 a => /home/a 如果什么都不写  /home
         component: SecondHome // 默认的二级路由组件
+      }, {
+      //   path:'/home/comment', // 完整地址 评论列表的地址
+        path: 'comment', // 简写地址
+        component: () => import('@/views/comment') // 按需加载的方式
       }
     ]
     // 在一级路由的路由表下 children 配置二级路由表
---
+++ src/views/comment/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      我是谁!我是评论
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
