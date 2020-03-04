11a1c91c5bd26b9f475ec538ab472d841a4667d4
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-04 16:11:46 +0800

    评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -18,9 +18,14 @@
        <el-table-column  prop="total_comment_count" label="总评论数"></el-table-column>
        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
        <el-table-column  label="操作">
+          <!-- el-table-column组件 在插槽中传出了 row $index store column -->
+          <!-- 插槽 -> 作用域插槽 ->子组件中的数据 通过插槽传出 slot-scope接收  row(行数据) $index(索引)  -->
           <!-- 可以放组件 -->
-          <el-button size="small" type='text'>修改</el-button>
-          <el-button size="small" type='text'>关闭评论</el-button>
+          <template slot-scope="obj">
+            <el-button size="small" type='text'>修改</el-button>
+             <!-- 文本内容要根据 当前行里面的评论状态决定显示还是隐藏 -->
+            <el-button @click="openOrClose(obj.row)" size="small" type='text'>{{ obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
+          </template>
 
        </el-table-column>
     </el-table>
@@ -60,6 +65,34 @@ export default {
       // index 代表当前的索引
       // 该函数需要返回一个值 用来显示
       return cellValue ? '正常' : '关闭'
+    },
+    //  打开或者关闭逻辑
+    openOrClose (row) {
+      const mess = row.comment_status ? '关闭' : '打开'
+      // $confirm 也支持 promise 点击确定会进入到then 点击取消会进入到catch
+      this.$confirm(`是否确定${mess}评论`, '提示').then(() => {
+        // 调用打开或者关闭接口
+        this.$axios({
+          url: '/comments/status', // 请求地址
+          method: 'put', // 请求类型
+          // query参数
+          params: {
+            article_id: row.id // 要求参数的文章id
+          },
+          data: {
+            //  body参数
+            allow_comment: !row.comment_status // 是打开还是关闭  此状态和评论状态相反
+          }
+        }).then(() => {
+          //   成功了 提示个消息 然后 重新拉取数据4
+          this.$message.success(`${mess}评论成功`)
+          //  重新拉取数据
+          this.getComment() // 调用重新拉取数据的方法
+        }).catch(() => {
+          //   表示失败了 会进入到catch
+          this.$message.error(`${mess}评论失败`)
+        })
+      })
     }
   },
   created () {
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- 先在最外层放置一个大容器 因为 小容器只能放到大容器里面 -->
    <el-container>
-     <el-aside style="width:230px">
+     <el-aside style="width:230px;background:#2e2f32">
        <!-- 左侧导航组件 -->
        <layout-aside></layout-aside>
      </el-aside>
