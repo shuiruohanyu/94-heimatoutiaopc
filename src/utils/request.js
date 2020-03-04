576ac18b264d5cce156079416c118aa462dddcde
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-04 11:09:56 +0800

    axios拦截器-统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@
  * 配置拦截器 以及其他
  * ****/
 import axios from 'axios'
+import router from '@/router' // 路由实例对象
 // 拦截器及其他操作
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
 
@@ -27,7 +28,22 @@ axios.interceptors.response.use(function (response) {
   // 在拦截器中 需要将数据返回  将数据进行解构
   return response.data ? response.data : {} // 有的接口 没有任何的响应数据
   // 成功的时候执行
-}, function () {
+}, function (error) {
   // 失败的时候执行
+  // error是错误对象 里面包含了错误的状态码 和响应信息
+  // 401 状态码  表示用户认证失败 用户身份不对
+  // 401出现的时候 表示 拿错钥匙/钥匙过期/钥匙没拿/钥匙名不对/钥匙格式不对 ...
+  // 之前的导航守卫  校验了token有没有 检查了钥匙有没有
+  // 应该换一个新钥匙 项目1 讲一种比较粗暴的换钥匙 项目2 讲一种比较温柔的钥匙
+  // 粗暴的换钥匙 回登录页 => 重新登录换一把新的 重来
+  // 回登录页之前 应该把旧钥匙给清除掉
+  if (error.response.status === 401) {
+    localStorage.removeItem('user-token') // 删除钥匙
+    router.push('/login') // 直接导入路由实例对象 使用跳转方式 和组件中this.$router是一样的
+    // 跳回登录页
+    // this.$router 错误的!!! this 不是组件实例
+  }
+  // 进行错误的处理
+  return Promise.reject(error) // 所有的axios调用 依然可以在catch中得到错误
 })
 export default axios
