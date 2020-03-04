4ff396e87997a84ad8f9c329b157c0100e571d6d
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-04 15:10:33 +0800

    -评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -12,11 +12,17 @@
     <el-table :data="list">
        <!-- 使用el-table-column作为列 -->
        <!-- prop 表示显示的字段 label表示表头 -->
-       <el-table-column width="600"  label="标题"></el-table-column>
-       <el-table-column  label="评论状态"></el-table-column>
-       <el-table-column  label="总评论数"></el-table-column>
-       <el-table-column  label="粉丝评论数"></el-table-column>
-       <el-table-column  label="操作"></el-table-column>
+       <el-table-column prop="title" width="600"  label="标题"></el-table-column>
+       <!-- 给el-table-column 一个formatter属性 用来格式化内容 因为table中不显示布尔值-->
+       <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+       <el-table-column  prop="total_comment_count" label="总评论数"></el-table-column>
+       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+       <el-table-column  label="操作">
+          <!-- 可以放组件 -->
+          <el-button size="small" type='text'>修改</el-button>
+          <el-button size="small" type='text'>关闭评论</el-button>
+
+       </el-table-column>
     </el-table>
  </el-card>
 </template>
@@ -29,6 +35,36 @@ export default {
 
       ]
     }
+  },
+  methods: {
+    //   获取评论数据
+    getComment () {
+      this.$axios({
+        url: '/articles', // 请求地址
+        params: {
+          response_type: 'comment' // 此参数用来控制获取数据类型
+        }
+        // query参数应该在哪个位置传 axios
+        // params 传get参数也就是query参数
+        // data   传body参数也就是请求体参数
+      }).then(result => {
+        //  将返回结果的中 数组 给list
+        this.list = result.data.results
+      })
+    },
+    //  定义一个格式化的函数
+    formatterBool (row, column, cellValue, index) {
+      //  row 代表当前的一行数据
+      // column 代表当前的列信息
+      // cellValue 代表当前单元格的值
+      // index 代表当前的索引
+      // 该函数需要返回一个值 用来显示
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    //   在钩子函数中 直接获取数据
+    this.getComment()
   }
 }
 </script>
