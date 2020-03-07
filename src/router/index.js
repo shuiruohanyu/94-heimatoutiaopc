404234e7c74dcabb1da5ee1e0f381d2f2cdd76bb
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-07 11:29:07 +0800

    内容列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,9 @@ const routes = [
       //  path: '/home/material' // 二级路由地址
         path: 'material',
         component: () => import('@/views/material') // 简写
+      }, {
+        path: 'articles',
+        component: () => import('@/views/articles') // 引入组件
       }
     ]
     // 在一级路由的路由表下 children 配置二级路由表
