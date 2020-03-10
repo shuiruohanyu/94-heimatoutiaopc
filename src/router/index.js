0f019c348eba26c017f14e40e231bdf4a0cddf84
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-10 16:07:01 +0800

    404页面应用

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '*', // 匹配任何找不到组件的路由
+    component: () => import('@/views/404')
+  },
+  {
     path: '/',
     redirect: '/home' // 强制跳转到home页
   },
