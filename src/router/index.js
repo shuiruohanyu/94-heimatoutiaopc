726ef7d2fafa3cd7c1077ece7bda7b65f30ed770
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-10 14:34:59 +0800

    账户信息-新增页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,9 @@ const routes = [
       }, {
         path: 'publish/:articleId?', // 发布文章 加上问号表示 可有可无
         component: () => import('@/views/publish') // 引入组件
+      }, {
+        path: 'account', // 账户地址
+        component: () => import('@/views/account') // 账户组件
       }
     ]
     // 在一级路由的路由表下 children 配置二级路由表
---
+++ src/views/account/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-card>
+      <!-- 账户信息 -->
+      <bread-crumb slot='header'>
+        <template slot='title'>账户信息</template>
+      </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
